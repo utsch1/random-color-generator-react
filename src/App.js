6e56a6c4b642cae9b40d9951b946376286d07d9a
Ute <utegreiner93@gmail.com>
2022-09-12 19:06:34 +0200

    Add color generator, hue, luminosity, parts layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import { Gradient } from 'javascript-color-gradient';
+import randomColor from 'randomcolor';
+import { useState } from 'react';
 
 function App() {
+  const [color, setColor] = useState(randomColor());
+  const [userHue, setUserHue] = useState('');
+  const [userLum, setUserLum] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Layout">
+      <div className="Headline">
+        <h1>Random Color Generator</h1>
+      </div>
+      <br />
+      <div
+        style={{
+          margin: '0 auto',
+          width: 700,
+          height: 400,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontSize: '20px',
+          border: '1px solid black',
+          backgroundColor: color,
+        }}
+      >
+        <h2>Generated color: {color}</h2>
+      </div>
+      {/* Generate Button */}
+      <button
+        onClick={() => {
+          setColor(randomColor());
+        }}
+      >
+        Generate
+      </button>
+      <br /> <br />
+      {/* Color Input */}
+      <p>Color</p>
+      <input
+        value={userHue}
+        onChange={(event) => {
+          setUserHue(event.currentTarget.value);
+          const chosenHue = event.currentTarget.value;
+          if (
+            chosenHue === 'red' ||
+            chosenHue === 'orange' ||
+            chosenHue === 'yellow' ||
+            chosenHue === 'green' ||
+            chosenHue === 'blue' ||
+            chosenHue === 'purple' ||
+            chosenHue === 'pink'
+          ) {
+            setColor(
+              randomColor({
+                luminosity: 'random',
+                hue: chosenHue,
+              }),
+            );
+          }
+        }}
+      />
+      <br />
+      {/* Luminosity Input */}
+      <p>Luminosity</p>
+      <input
+        value={userLum}
+        onChange={(event) => {
+          setUserLum(event.currentTarget.value);
+          const chosenLum = event.currentTarget.value;
+          if (chosenLum === 'light' || chosenLum === 'dark') {
+            setColor(
+              randomColor({
+                luminosity: chosenLum,
+                hue: 'random',
+              }),
+            );
+          }
+        }}
+      />
     </div>
   );
 }
